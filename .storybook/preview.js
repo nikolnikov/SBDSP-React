ba9460628c9697fee90ab9951d6530e14498375e
Nick Sacy <nicholas.a.sacy@questdiagnostics.com>
2025-02-20 12:12:22 -0600

    DSP react sb wip

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,28 +1,23 @@
 import { themes } from '@storybook/theming';
 import '!style-loader!css-loader!sass-loader!/src/stories/assets/css/overrides.css';
-import '!style-loader!css-loader!sass-loader!/src/stories/assets/css/ds-story.scss';
+import '!style-loader!css-loader!sass-loader!/src/stories/assets/css/dsp.scss';
 
-/** @type { import('@storybook/react').Preview } */
-const preview = {
-    parameters: {
-        controls: {
-            matchers: {
-                color: /(background|color)$/i,
-                date: /Date$/i
-            }
-        },
-        docs: {
-            source: {
-                theme: themes.dark
-            }
-        },
-        options: {
-            storySort: {
-                order: 'alphabetical',
-                locales: 'en-US'
-            }
+const parameters = {
+    controls: {
+        matchers: {
+            color: /(background|color)$/i,
+            date: /Date$/i
+        }
+    },
+    docs: {
+        source: {
+            theme: themes.dark
+        }
+    },
+    options: {
+        storySort: {
+            order: 'alphabetical',
+            locales: 'en-US'
         }
     }
 };
-
-export default preview;
