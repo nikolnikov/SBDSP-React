4895555d4dea22d9d74ecc933e3e5a9044de4f9c
Nick Sacy <nicholas.a.sacy@questdiagnostics.com>
2025-02-21 08:35:25 -0600

    DSP react sb wip

--- src/stories/Button/IconButton.stories.js
+++ src/stories/Button/IconButton.stories.js
@@ -23,14 +23,34 @@ export default {
     tags: ['autodocs']
 };
 
-export const Primary = {
-    args: {
-        iconClass: 'ds-icon--gear',
-        size: 'large',
-        type: 'icon'
-    },
-    argTypes: {
-        customClasses: { table: { disable: true } },
-        clickHandler: { table: { disable: true } }
-    }
+const Button = props => <IconButton {...props} />;
+
+const Template = args => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    iconClass: 'ds-icon--gear',
+    size: 'large',
+    type: 'icon'
 };
+Primary.argTypes = {
+    customClasses: { table: { disable: true } },
+    clickHandler: { table: { disable: true } }
+};
+Primary.displayName = 'Icon Button';
+
+export const Inverse = Template.bind({});
+Inverse.args = {
+    iconClass: 'ds-icon--gear',
+    isInverse: true,
+    size: 'large',
+    type: 'icon'
+};
+Inverse.argTypes = {
+    customClasses: { table: { disable: true } },
+    clickHandler: { table: { disable: true } }
+};
+Inverse.displayName = 'Inverse icon Button';
+// Inverse.parameters = {
+//     backgrounds: { values: { name: 'Dark' } }
+// };
