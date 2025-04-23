396bf165610d775a92e32fca4498e5e08c26dfd9
Nick Sacy <nicholas.a.sacy@questdiagnostics.com>
2025-04-23 14:51:36 -0500

    Cleanup

--- src/stories/Datepicker/Datepicker.jsx
+++ src/stories/Datepicker/Datepicker.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import DSDatePicker from '../components/DatePicker'
+import DSDatepicker from '../components/Datepicker'
 import classNames from 'classnames';
 import PropTypes from 'prop-types';
 
@@ -22,7 +22,7 @@ export const QDSDatepicker = ({
     showOnTop = false
 }) => {
     return (
-        <DSDatePicker
+        <DSDatepicker
             customClasses={classNames(customClasses)}
             errorMessage={errorMessage}
             hasError={hasError}
