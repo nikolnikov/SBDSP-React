c2230085d10d7825653ca64881036d4cfc4b788d
Nick Sacy <nicholas.a.sacy@questdiagnostics.com>
2025-07-01 14:20:06 -0700

    Cleanup

--- src/stories/Divider/Divider.jsx
+++ src/stories/Divider/Divider.jsx
@@ -3,7 +3,7 @@ import classNames from "classnames";
 import PropTypes from 'prop-types';
 
 export const QDSDivider = ({ 
-    color, 
+    color = green__800, 
     customClasses
 }) => {
     return (
